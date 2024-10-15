d57ee22eda4cd2da59c1e9a96339422e9cdec103
Akashjadhav18 <akashijadhav18@gmail.com>
2024-10-15 15:40:48 +0530

    Add files via upload

---
+++ USN.java
@@ -0,0 +1,13 @@
+public class USN {
+    public static void main(String[] args) {
+                            for(int i=1;i<10;i++){
+                   System.out.println("4MW23CS00"+i);
+                        
+}
+                   for(int i=10;i<65;i++){
+                   System.out.println("4MW23CS0"+i);
+                        
+}
+
+}
+}
